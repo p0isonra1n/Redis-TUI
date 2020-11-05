15df91e9e667d07dfab41dd3d862af2478051075
Isaac Parker <isaacparker@gmail.com>
2020-11-05 16:02:53 +1000

    Add tryDetectJSON to display JSON values as pretty printed JSON. Needs more work

--- Program.cs
+++ Program.cs
@@ -18,12 +18,16 @@ namespace Redis_TUI
         static TextView textView;
 
         static ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        static Toplevel top;
+
+
+        static string selectedKeyValue = "";
 
 
         static void Main(string[] args)
         {
             Application.Init();
-            var top = Application.Top;
+            top = Application.Top;
 
             // Creates the top-level window to show
             var winNodes = new Window("Nodes")
@@ -67,26 +71,28 @@ namespace Redis_TUI
 			}),
             new MenuBarItem ("_Edit", new MenuItem [] {
                 new MenuItem ("_Copy", "", null),
-                new MenuItem ("C_ut", "", null),
+                new MenuItem ("_Cut", "", null),
                 new MenuItem ("_Paste", "", null)
+            }),
+            new MenuBarItem ("_View", new MenuItem [] {
+                new MenuItem ("_As Text", "", viewAsText),
+                new MenuItem ("_As JSON", "", viewAsJSON)
+            }),
+            new MenuBarItem ("_Key", new MenuItem [] {
+                new MenuItem ("_Add", "", addKey),
+                new MenuItem ("_Delete", "", deleteKey)
             })
             });
             top.Add(menu);
 
             try
             {
-                IServer server = redis.GetServer("localhost", 6379);
-                foreach (var key in server.Keys())
-                {
-                    listOfNames.Add(key);
-                }
 
+                loadKeys();
                 listView = new ListView(new Rect(0, 0, top.Frame.Width / 5, 200), listOfNames);
                 listView.OpenSelectedItem += onItemClick;
-                textView = new TextView() { X = 0 , Y = 0, Width = Dim.Fill(), Height = Dim.Fill() };
+                textView = new TextView() { X = 0, Y = 0, Width = Dim.Fill(), Height = Dim.Fill() };
 
-                var labelNodes = new Label("Nodes: ") { X = 0, Y = 0 };
-                var labelValue = new Label("Value: ") { X = Pos.Right(listView), Y = 0 };
 
                 winNodes.Add(listView);
                 winValues.Add(textView);
@@ -102,11 +108,108 @@ namespace Redis_TUI
         static void onItemClick(ListViewItemEventArgs lviea)
         {
             IDatabase db = redis.GetDatabase();
-            //MessageBox.Query("Is this right", (string)db.StringGet((string)lviea.Value), "Yes", "no");
-            //listOfNames.Insert(lviea.Item + 1, "->TESTTST");
-            //listView.MoveDown();
-            //textView.Text = PrettyJson((string)db.StringGet((string)lviea.Value));
-            textView.Text = (string)db.StringGet((string)lviea.Value);
+            selectedKeyValue = db.StringGet((string)lviea.Value);
+            textView.Text = tryDetectJSON(selectedKeyValue);
+        }
+
+        static void deleteKey()
+        {
+            int selectedKey = listView.SelectedItem;
+            IDatabase db = redis.GetDatabase();
+            db.KeyDelete(listOfNames[selectedKey]);
+            loadKeys();
+        }
+
+        static void addKey()
+        {
+            bool okpressed = false;
+            Action okay = () => { Application.RequestStop(); okpressed = true; };
+            var ok = new Button(3, 14, "Ok")
+            {
+                //Clicked = okay
+            };
+            ok.Clicked += () => { Application.RequestStop(); okpressed = true; };
+            var cancel = new Button(10, 14, "Cancel")
+            {
+
+            };
+            cancel.Clicked += () => Application.RequestStop();
+            var dialog = new Dialog("Add Key", 60, 18, ok, cancel);
+
+            var entryKey = new TextField()
+            {
+                X = 1,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = 1
+            };
+            var entryValue = new TextField()
+            {
+                X = 1,
+                Y = 2,
+                Width = Dim.Fill(),
+                Height = 1
+            };
+            dialog.Add(entryKey);
+            dialog.Add(entryValue);
+            Application.Run(dialog);
+            if (okpressed)
+            {
+                IDatabase db = redis.GetDatabase();
+                db.StringSet(entryKey.Text.ToString(), entryValue.Text.ToString());
+                loadKeys();
+            }
+
+        }
+
+        static void loadKeys()
+        {
+            IServer server = redis.GetServer("localhost", 6379);
+            listOfNames.Clear();
+            foreach (var key in server.Keys())
+            {
+                listOfNames.Add(key);
+            }
+            if (listView != null)
+            {
+                if (listView.SelectedItem == 0)
+                {
+                    listView.MoveDown();
+                }
+                else
+                {
+                    listView.MoveUp();
+                }
+            }
+
+        }
+
+        static void viewAsText()
+        {
+            textView.Text = selectedKeyValue;
+        }
+        static void viewAsJSON()
+        {
+            try
+            {
+                textView.Text = PrettyJson(selectedKeyValue);
+            }
+            catch (Exception e)
+            {
+                MessageBox.ErrorQuery("ERROR", e.ToString(), "Okay");
+            }
+
+        }
+
+        static string tryDetectJSON(string input)
+        {
+            try
+            {
+                return PrettyJson(input);
+            }catch(Exception e)
+            {
+                return input;
+            }
         }
         public static string PrettyJson(string unPrettyJson)
         {
